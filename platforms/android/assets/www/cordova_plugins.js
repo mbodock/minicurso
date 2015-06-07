f301181ccb73ccc3a8effa94c98a99e9e4e205b6
Mbodock (Marcus Vinicius) <mbodock@gmail.com>
2015-06-07 11:28:51 -0300

    Adiciona jquery e plugin de vibrar

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,22 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-vibration": "1.1.0",
+    "cordova-plugin-console": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
